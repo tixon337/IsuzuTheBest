8362f51697f572b0d63c131fcafd50c032b42524
Igor <kozlovigor1989@gmail.com>
2020-07-23 16:54:11 +0300

    Release configurator

--- public/js/application.js
+++ public/js/application.js
@@ -1,40 +1,51 @@
-const form = document.getElementById('configForm')
-const sum = document.querySelector('.sum')
-
-// const selected = document.querySelectorAll('.select')
-
+const sum = document.querySelector(".sum");
+const selected = document.querySelectorAll('.select')
+const buttonSum = document.getElementById('buttonSum')
 
 
-const engineImport = document.getElementById('engine');
-const carcassImport = document.getElementById('carcass');
-const transmittionImport = document.getElementById('transmission');
-const colorImport = document.getElementById('color');
-let engine = engineImport.options[engineImport.selectedIndex].text;
-let carcass = carcassImport.options[carcassImport.selectedIndex].text;
-let transmittion = transmittionImport.options[transmittionImport.selectedIndex].text;
-let color = colorImport.options[colorImport.selectedIndex].text;
-sum.innerText = `${engine + carcass + transmittion + color}`;
+let engine = selected[0].options[selected[0].selectedIndex].text;
+let carcass = selected[1].options[selected[1].selectedIndex].text;
+let transmission = selected[2].options[selected[2].selectedIndex].text;
+let color = selected[3].options[selected[3].selectedIndex].text;
+sum.innerText = `Стоимость: 623852 Руб.`
 
-engineImport.addEventListener('change', () => {
-  engine = engineImport.options[engineImport.selectedIndex].text
-  sum.innerText = `${engine + carcass + transmittion + color}`;  
-  
-})
-carcassImport.addEventListener('change', () => {
-  carcass = carcassImport.options[carcassImport.selectedIndex].text  
-  sum.innerText = `${engine+carcass+transmittion+color}`;
-  
-})
-transmittionImport.addEventListener('change', () => {
-  transmittion = transmittionImport.options[transmittionImport.selectedIndex].text
-  console.log(transmittion);
-  sum.innerText = `${engine+carcass+transmittion+color}`;
-  
-})
-colorImport.addEventListener('change', () => {
-  color = colorImport.options[colorImport.selectedIndex].text
-  console.log(color);
-  sum.innerText = `${engine+carcass+transmittion+color}`;  
-})
+
+
+  buttonSum.addEventListener("click", async () => {    
+    let engine = selected[0].options[selected[0].selectedIndex].text;
+    let carcass = selected[1].options[selected[1].selectedIndex].text;
+    let transmission = selected[2].options[selected[2].selectedIndex].text;
+    let color = selected[3].options[selected[3].selectedIndex].text;      
+    const response = await fetch("/configurator/getSum", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        engine,
+        carcass,
+        transmission,
+        color,
+      }),
+    });
+    let responseObj = await response.json();
+
+    sum.innerText = `Стоимость: ${responseObj.price} Руб.`;
+  });
+
+
+
+
+// const engineImport = document.getElementById("engine");
+// const carcassImport = document.getElementById("carcass");
+// const transmittionImport = document.getElementById("transmission");
+// const colorImport = document.getElementById("color");
+// let engine = engineImport.options[engineImport.selectedIndex].text;
+// let carcass = carcassImport.options[carcassImport.selectedIndex].text;
+// let transmittion = transmittionImport.options[transmittionImport.selectedIndex].text;
+// let color = colorImport.options[colorImport.selectedIndex].text;
+// sum.innerText = `${engine + carcass + transmittion + color}`;
+
+
 
 
--- routes/configurator.js
+++ routes/configurator.js
@@ -16,10 +16,25 @@ router.get("/", async function (req, res) {
     const color = await ColorModel.find();
     const configuration = await ConfigurationModel.find();
     const data = { engine, carcass, transmission, color, configuration };
-    console.log(data.engine);
+    // console.log(data.engine);
     res.render("configurator", data);
   } catch (error) {
     console.log(error);
+  }
+});
+
+router.post("/getSum", async function (req, res) {
+  try {
+    const configuration = await ConfigurationModel.find({
+      "engine.name": req.body.engine,
+      "carcass.name": req.body.carcass,
+      'transmission.name': req.body.transmission,
+      'color.name': req.body.color,
+    });
+    console.log(configuration.length);
+    res.json({ price: configuration[0].price });
+  } catch (error) {
+    res.json({ error: "Такой комплектации не существует" });
   }
 });
 
