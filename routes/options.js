1295d21ea4827f30449d0afbde17354c4d8c8e78
tanyakuragina <kuragina_ta@mail.ru>
2020-07-23 12:09:06 +0300

    select names were added

--- routes/options.js
+++ routes/options.js
@@ -1,21 +1,21 @@
-const express  = require('express')
-const {ConfigurationModel}  = require('../DataBase/Database.js');
+const express = require("express");
+const { ConfigurationModel } = require("../DataBase/Database.js");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('addVariantOpt')
-})
+router.get("/options", async (req, res) => {
+  await res.render("addVariantOpt");
+});
 
-router.post('/', (req, res) => {
-  res.render('addVariantOpt')
-})
+router.post("/options", async (req, res) => {
+  await res.render("addVariantOpt");
+});
 
-router.put('/', (req, res) => {
-  res.render('changeVariantOpt')
-})
+// router.put("/", (req, res) => {
+//   res.render("changeVariantOpt");
+// });
 
-router.delete('/', (req, res) => {
-  res.render('deleteVariantOpt')
-})
+// router.delete("/", (req, res) => {
+//   res.render("deleteVariantOpt");
+// });
 
 module.exports = router;
