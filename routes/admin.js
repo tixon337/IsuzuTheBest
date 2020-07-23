d17cf283faf0e4823e44fd4d6a7eb6e6c1dbd9a3
Tembot <maremshaov.t@yandex.ru>
2020-07-23 18:02:56 +0300

    addnewoptionsHBS

--- routes/admin.js
+++ routes/admin.js
@@ -1,24 +1,21 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const { sessionChecker } = require('../middleware/auth.js')
+const { sessionChecker } = require("../middleware/auth.js");
 
+router.get("/", sessionChecker, (req, res) => {
+  res.render("admin");
+});
 
-router.get('/', sessionChecker, (req, res) => {
-  res.render('admin')
-})
+router.get("/add", sessionChecker, (req, res) => {
+  res.render("addAdmin");
+});
 
-router.get('/add', sessionChecker, (req, res) => {
-  res.render('addAdmin')
-})
-
-router.post('/add', sessionChecker, (req, res) => {
-  res.render('addAdmin')
-})
-
-
+router.post("/add", sessionChecker, (req, res) => {
+  res.render("addAdmin");
+});
 
 // router.post('/', sessionChecker, (req, res) => {
-  
+
 //   res.render('admin')
 // })
 
--- routes/options.js
+++ routes/options.js
@@ -10,14 +10,9 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    const engine = await EngineModel.find();
-    const carcass = await CarcassModel.find();
-    const transmission = await TransmissionModel.find();
-    const color = await ColorModel.find();
-    const configuration = await ConfigurationModel.find();
-    const data = { engine, carcass, transmission, color, configuration };
+    const data = {};
     console.log(data.engine);
-    res.render("addVariantOpt", data);
+    res.render("addVariantOpt");
   } catch (error) {
     console.log(error);
   }
@@ -25,17 +20,46 @@ router.get("/", async (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
-    const engine = await EngineModel.find();
-    const carcass = await CarcassModel.find();
-    const transmission = await TransmissionModel.find();
-    const color = await ColorModel.find();
-    const configuration = await ConfigurationModel.find();
-    const data = { engine, carcass, transmission, color, configuration };
+    const data = {};
     console.log(data.engine);
-    res.render("addVariantOpt", data);
+    res.render("addVariantOpt");
   } catch (error) {
     console.log(error);
   }
+});
+
+router.post("/add", async (req, res) => {
+  console.log(req.body);
+  const engine = await EngineModel.find();
+  const carcass = await CarcassModel.find();
+  const transmission = await TransmissionModel.find();
+  const color = await ColorModel.find();
+  const configuration = await ConfigurationModel.find();
+  switch (req.body.options) {
+    case "carcass":
+      res.render("addNewVariantOptions", { carcass });
+      break;
+
+    case "color":
+      res.render("addNewVariantOptions", { color });
+      break;
+
+    case "engine":
+      res.render("addNewVariantOptions", { engine });
+      break;
+
+    case "transmission":
+      res.render("addNewVariantOptions", { transmission });
+      break;
+
+    default:
+      break;
+  }
+});
+
+router.post("/addoptions", async (req, res) => {
+  console.log(req.body);
+  res.redirect("/options");
 });
 
 router.put("/", (req, res) => {
